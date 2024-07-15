2bfc4a25be4b31b3ebf2e223e28ed6dbea6e7ec9
hardfau1t <hardfau1t@proton.me>
2024-07-15 11:57:57 +0530

    refactor(main): remove previous logic start with clean

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod constants;
 mod executor;
+mod parser;
 mod store;
 
 use clap::Parser;
@@ -20,13 +21,10 @@ struct Arguments {
     #[arg(short, long)]
     list: bool,
     #[arg(required_unless_present("list"))]
-    service: Option<String>,
-    #[arg()]
-    #[arg(required_unless_present("list"))]
-    endpoint: Option<String>,
+    endpoint: Option<Vec<String>>,
     /// arguments for hooks, note to make it unamgious add -- before providing any flags
     /// add another -- separator to separate between prehook flags and post hook flags
-    #[arg(trailing_var_arg(true), allow_hyphen_values(true))]
+    #[arg(allow_hyphen_values(true), last(true))]
     args: Vec<String>,
 }
 
@@ -51,54 +49,8 @@ fn main() -> Result<(), anyhow::Error> {
         .with_writer(std::io::stderr)
         .init();
     debug!("Log level set to : {log_level:?}");
-    debug!(extra_args=?args.args);
-    let document = toml::from_str::<Document>(&std::fs::read_to_string(&args.config_file)?)?;
-    info!(file=?args.config_file, "parsed succesfully");
-    if args.list {
-        // if service name is present then then list end points
-        if let Some(service_name) = args.service {
-            if let Some(svc) = document.get_service(&service_name) {
-                // if endpoint is present then dump the content of endpoint
-                if let Some(ep_name) = args.endpoint {
-                    if let Some(ep) = svc.get_endpoint(&ep_name) {
-                        println!("endpoint: {:#?}", ep);
-                        Ok(())
-                    } else {
-                        error!(service = service_name, "Couldn't find service");
-                        Err(anyhow::anyhow!("Failed to list services"))
-                    }
-                } else {
-                    svc.endpoint.iter().for_each(|ep| {
-                        println!(
-                            "--> name: {}, alias: {}",
-                            ep.name,
-                            ep.alias.as_deref().unwrap_or("null")
-                        )
-                    });
-                    Ok(())
-                }
-            } else {
-                error!(service = service_name, "Couldn't find service");
-                Err(anyhow::anyhow!("Failed to list services"))
-            }
-        } else {
-            // list services since service name is not present
-            document
-                .services
-                .iter()
-                .for_each(|svc| println!("-> name: {}, alias: {}", svc.name, svc.alias.as_deref().unwrap_or("null")));
-            Ok(())
-        }
-    } else {
-        let Some(service_name) = args.service else {
-            error!("service is required field, unless listing");
-            return Err(anyhow::anyhow!("missing param"));
-        };
-        let Some(endpoint_name) = args.endpoint else {
-            error!("service is required field, unless listing");
-            return Err(anyhow::anyhow!("missing param"));
-        };
-        let flags = args.args.iter().map(|arg| arg.as_str()).collect::<Vec<_>>();
-        parse_and_exec_service(&document, &service_name, &endpoint_name, flags.as_slice())
-    }
+    debug!(extra_args=?args.args, "Arguments for the scripts");
+
+    let config = parser::Config::open(&args.config_file)?;
+    Ok(())
 }
