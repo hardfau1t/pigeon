f58c8b10d47b0720a0083ee3ab8b8e5c8b34e551
hardfau1t <hardfau1t@proton.me>
2024-10-14 07:37:20 +0530

    feat(hook): better hook errors
    Show hook failures and error messages

--- src/hook.rs
+++ src/hook.rs
@@ -1,7 +1,7 @@
 use miette::{Context, IntoDiagnostic};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::{borrow::Borrow, io::Write};
-use tracing::{debug, info, instrument, trace};
+use std::{borrow::Borrow, io::Write, os::unix::process::ExitStatusExt};
+use tracing::{debug, error, instrument, trace};
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
 #[serde(deny_unknown_fields)]
@@ -28,6 +28,7 @@ impl Hook {
             Hook::Closure(_cl) => unimplemented!("Currently closures are not supported"),
             Hook::Path(path) => {
                 debug!("Executing hook: {path:?}");
+                // setup child to take stdin and return both stdout and stdin
                 let mut child = std::process::Command::new(path)
                     .stdin(std::process::Stdio::piped())
                     .stdout(std::process::Stdio::piped())
@@ -36,7 +37,10 @@ impl Hook {
                     .spawn()
                     .into_diagnostic()
                     .wrap_err("Couldn't run hook")?;
+
                 debug!("writing to child: {body_buf:x?}");
+
+                // execute child with input
                 child
                     .stdin
                     .take()
@@ -44,18 +48,32 @@ impl Hook {
                     .write_all(&body_buf)
                     .into_diagnostic()
                     .wrap_err("Failed to send body to hook")?;
+
+                // collect child output
                 let output = child
                     .wait_with_output()
                     .into_diagnostic()
                     .wrap_err("Failed to read hook output")?;
                 debug!("pre-hook output: {:x?}", output.stdout);
-                info!(
-                    "pre-hook stderr: `{}`",
-                    String::from_utf8_lossy(&output.stderr)
-                );
+
+                // assuming stderr to be utf-8
+                let child_stderr = String::from_utf8_lossy(&output.stderr);
+
+                if !child_stderr.is_empty() {
+                    error!("pre-hook stderr: `{}`", child_stderr);
+                }
+                // check if the execution is success or not
+                if !output.status.success() {
+                    let code =
+                        std::process::ExitStatus::from_raw(output.status.code().unwrap_or(1));
+                    miette::bail!("hook exited with error: {code}")
+                }
+
+                // deserialize output and read from stdout
                 let pre_hook_obj: T = rmp_serde::from_slice(output.stdout.as_ref())
                     .into_diagnostic()
                     .wrap_err("Failed to deserialize output of hooks")?;
+
                 Ok(pre_hook_obj)
             }
         }
